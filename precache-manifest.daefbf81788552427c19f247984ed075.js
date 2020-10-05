0cee71f3c37570b7c0c17b73b4bc80288ad6d4f3
trey <treymiller@protonmail.com>
2020-10-04 17:03:37 -0700

    Updates

---
+++ precache-manifest.daefbf81788552427c19f247984ed075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3cc4cd4d055f857ad05a9083f1a554",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9a53661c4f41040e84a",
+    "url": "./static/css/main.2824927f.chunk.css"
+  },
+  {
+    "revision": "44940e1134c4568d1e59",
+    "url": "./static/js/2.4fb664b9.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.4fb664b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a53661c4f41040e84a",
+    "url": "./static/js/main.ae9085d1.chunk.js"
+  },
+  {
+    "revision": "f3f05fa96700c887cb99",
+    "url": "./static/js/runtime-main.f828ee36.js"
+  }
+]);
